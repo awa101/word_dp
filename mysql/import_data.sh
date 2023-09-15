36ab9ecd9b6359b2440a1f25f56a6f8456a60614
Yedam101 <109948144+Yedam101@users.noreply.github.com>
2023-09-15 17:55:10 +0900

    dp10

--- mysql/import_data.sh
+++ mysql/import_data.sh
@@ -7,67 +7,66 @@ BASE_PATH="/csv"
 
 # KR tables
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/KrWord.csv' INTO TABLE KrWord
+LOAD DATA LOCAL INFILE '$BASE_PATH/KrWord.csv' INTO TABLE \`word_KrWord\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, hangeul, sound, numbering);
+(\`word\`, \`hangeul\`, \`sound\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/KrMeaning.csv' INTO TABLE KrMeaning
+LOAD DATA LOCAL INFILE '$BASE_PATH/KrMeaning.csv' INTO TABLE \`word_KrMeaning\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, meaning, order, numbering);
+(\`word\`, \`meaning\`, \`order\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/KrExample.csv' INTO TABLE KrExample
+LOAD DATA LOCAL INFILE '$BASE_PATH/KrExample.csv' INTO TABLE \`word_KrExample\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(meaning, example, order, word, numbering);
+(\`meaning\`, \`example\`, \`order\`, \`word\`, \`numbering\`);
 "
 
 # JP tables
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/JpWord.csv' INTO TABLE JpWord
+LOAD DATA LOCAL INFILE '$BASE_PATH/JpWord.csv' INTO TABLE \`word_JpWord\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, kana, sound, numbering);
+(\`word\`, \`kana\`, \`sound\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/JpMeaning.csv' INTO TABLE JpMeaning
+LOAD DATA LOCAL INFILE '$BASE_PATH/JpMeaning.csv' INTO TABLE \`word_JpMeaning\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, meaning, order, numbering);
+(\`word\`, \`meaning\`, \`order\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/JpExample.csv' INTO TABLE JpExample
+LOAD DATA LOCAL INFILE '$BASE_PATH/JpExample.csv' INTO TABLE \`word_JpExample\`
 FIELDS TERMINATED BY '\t' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(meaning, example, order, word, numbering);
+(\`meaning\`, \`example\`, \`order\`, \`word\`, \`numbering\`);
 "
-
 
 # CN tables
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/CnWord.csv' INTO TABLE CnWord
+LOAD DATA LOCAL INFILE '$BASE_PATH/CnWord.csv' INTO TABLE \`word_CnWord\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, sound, numbering);
+(\`word\`, \`sound\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/CnMeaning.csv' INTO TABLE CnMeaning
+LOAD DATA LOCAL INFILE '$BASE_PATH/CnMeaning.csv' INTO TABLE \`word_CnMeaning\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(word, meaning, order, numbering);
+(\`word\`, \`meaning\`, \`order\`, \`numbering\`);
 "
 
 mysql -u $USER -p$PASSWORD $DATABASE --local-infile=1 -e "
-LOAD DATA LOCAL INFILE '$BASE_PATH/CnExample.csv' INTO TABLE CnExample
+LOAD DATA LOCAL INFILE '$BASE_PATH/CnExample.csv' INTO TABLE \`word_CnExample\`
 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'
 LINES TERMINATED BY '\n' IGNORE 1 ROWS
-(meaning, example, order, word, numbering);
+(\`meaning\`, \`example\`, \`order\`, \`word\`, \`numbering\`);
 "
